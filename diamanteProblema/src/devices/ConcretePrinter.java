633ea300d6b36fd28ddd217d612992d454ebbc80
José Guiomar da Silva Jr <69606974+joseGjr@users.noreply.github.com>
2023-08-03 12:01:19 -0300

    Add files via upload

---
+++ diamanteProblema/src/devices/ConcretePrinter.java
@@ -0,0 +1,16 @@
+package devices;
+
+public class ConcretePrinter extends Device implements Printer{
+    public ConcretePrinter(String serialNumber) {
+        super(serialNumber);
+    }
+
+    @Override
+    public void processDoc(String doc) {
+        System.out.println("printing processing "+ doc);
+    }
+  @Override
+    public void print(String doc) {
+        System.out.println("printing: "+ doc);
+    }
+}
